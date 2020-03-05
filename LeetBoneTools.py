2dac991ae4d1ce709257363543986febf044cab6
Colin Leet <colinleet@gmail.com>
2020-03-05 17:41:22 -0500

    Updated readme.

--- LeetBoneTools.py
+++ LeetBoneTools.py
@@ -9,12 +9,14 @@
 
 # This blender add-on only runs in pose mode.
 
-# The user can choose to add cached bones to or replace their current selection.
+# This tool-set's panel and pie menu shows-up only when an armature is selected in pose mode:
+# the tool-set's panel is under 3D View -> Item,
+# and to use the pie menu hold control and left click in a 3d View.
+#
+# Cached bone selections are created, edited, and deleted only in the tool's 3d-view panel, not in the pie menu.
 # Cached bone selections are saved in an auxiliary text file in the same directory as the blend file.
-# New cached bone selections are created and edited only in the tool's 3d-view panel, not in the pie menu.
-
-# The panel and pie menu will only appear when an armature is selected in pose-mode.
-# To access this toolset’s pie menu hold control and click in a 3d-view when an armature is selected in pose-mode.
+# The user can choose to add to, or replace, their current selection with cached bone selections.
+# The user can also choose to focus on the selected bones when using the tool modify bone selections.
 
 # Original script layout derived from: https://blender.stackexchange.com/q/57306/3710
 
